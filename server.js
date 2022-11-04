b5dbd8a8d598097b32330bfc630fe63066b3d46c
Carl Bebli <beblicarl.cb@gmail.com>
2022-11-04 04:33:41 +0000

    added the openai to task

--- server.js
+++ server.js
@@ -1,27 +1,57 @@
 const express = require('express');
-const app = express();
+const app = express()
 const cors = require('cors')
+require('dotenv').config()
 const PORT = 3000
 
 app.use(cors())
 app.use(express.json())
 
+const { Configuration, OpenAIApi } = require("openai");
+
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+})
+const openai = new OpenAIApi(configuration);
+
+
 app.get('/', (req, res) => {
     res.send("Hello World")
   })
 
-app.post('/calculation' , (req, res) => {
+app.post('/calculation' ,  async (req, res) => {
   const {operation_type , x , y} = req.body
-  const basicOp = (operation_type, x, y) =>
-  ({
-    "addition": x + y,
-    "subtraction": x - y,
-    "multiplication": x * y,
-    "division": x / y,
-  }[operation_type.value])
+ 
+  let response
+  
+  if( x && y ){
+    const basicOp = (operation_type, x, y) =>
+        ({
+          "addition": x + y,
+          "subtraction": x - y,
+          "multiplication": x * y,
+          "division": x / y,
+        }[operation_type.value])
+
+  response = basicOp(operation_type, x, y)
+  }
+  else {
+  response = await openai.createCompletion({
+    model: "text-davinci-002",
+    prompt: operation_type,
+    temperature: 0.7,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  })
+  const data = response.data.choices[0].text.trimStart()
+  response = parseInt(data.split("=")[1])
+}
 
 
-  res.json({ 'slackUsername': 'beblicarl', "operation_type" : operation_type.value, result: basicOp(operation_type, x, y) })
+  res.status(201).json({ 'slackUsername': 'beblicarl' , "operation_type" : operation_type?.value , result : response})
 })
 
 app.listen(process.env.PORT || PORT, () => {
